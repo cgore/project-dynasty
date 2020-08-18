a02e6950395e2b64035db6f141ad6cdb3227207b
Christopher Mark Gore <cgore@cgore.com>
2020-08-17 23:29:30 -0500

    getting out just the log text

--- src/dynasty/core.clj
+++ src/dynasty/core.clj
@@ -1,1 +1,17 @@
-(ns dynasty.core)
+(ns dynasty.core
+  (:require [dynasty.data :as data]
+            [dynasty.strings :as strings]))
+
+(def form-rift
+  (-> data/systems :formidine-rift (select-keys [:alpha :beta :gamma :delta])))
+
+(def form-rift-logs
+  (->> form-rift
+       (map (fn [[k v]] (-> v :logs)))
+       flatten
+       (map :text)
+       (filter (complement nil?))))
+
+(comment
+
+  )
