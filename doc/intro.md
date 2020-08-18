17700da8279fb6fd2056d628b89c350657696a2e
Christopher Mark Gore <cgore@cgore.com>
2020-08-17 22:31:42 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dynasty
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dynasty/core_test.clj
@@ -0,0 +1,7 @@
+(ns dynasty.core-test
+  (:require [clojure.test :refer :all]
+            [dynasty.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
