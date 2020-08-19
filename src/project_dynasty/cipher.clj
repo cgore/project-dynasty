46f3d13502a827c5d4a267e33758b5f0d1129570
Christopher Mark Gore <cgore@cgore.com>
2020-08-19 11:51:25 -0500

    default key for caesar to match the classic

--- src/project_dynasty/cipher.clj
+++ src/project_dynasty/cipher.clj
@@ -2,12 +2,19 @@
 
 (defn caesar
   "Assumes offset >=0, words entirely lowercase English characters or spaces"
-  [offset words]
+  ([words] ; classical Caesar shift was 3
+   (caesar 3 words))
+  ([offset words]
+   (let [alphabet-chars (map char "abcdefghijklmnopqrstuvwxyz")
+         alphabet-shifted (->> (cycle alphabet-chars) (take 100) (drop offset))
+         shifted-map (-> (zipmap alphabet-chars alphabet-shifted)
+                         (assoc \space \space))]
+     (apply str (map shifted-map (map char words))))))
+
+(defn keyed-caeser [key words]
   (let [alphabet-chars (map char "abcdefghijklmnopqrstuvwxyz")
+        
         alphabet-shifted (->> (cycle alphabet-chars) (take 100) (drop offset))
         shifted-map (-> (zipmap alphabet-chars alphabet-shifted)
                         (assoc \space \space))]
     (apply str (map shifted-map (map char words)))))
-
-(defn keyed-caeser [key words]
-  )
